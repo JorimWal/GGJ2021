d0ac48d9a627dc93239a605fee702962e46d808d
Emiliano Cenizo <emicenizo@gmail.com>
2021-01-29 23:53:47 +0100

    Setting Death tiles

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,14 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Map : MonoBehaviour {
 
+
+    public int mapSize = 10;
+
+    public int deathTiles = 5;
+
+    public const int MAX_TRIES_FOR_MAP_GENERATION = 5;
+
     private static Map _instance;
 
     public static Map Instance { get { return _instance; } }
 
     private Vector2Int playerPosition;
+    private Vector2Int treasurePosition;
+
+    private List<Vector2Int> dangerPositions;
 
     public TileMapController tileMapController;
 
@@ -27,7 +38,6 @@ public class Map : MonoBehaviour {
     }
 
 
-    public int mapSize = 10;
 
     private Dictionary<Vector2Int, string> grid;
     public Vector2Int getPlayerPosition()
@@ -41,22 +51,30 @@ public class Map : MonoBehaviour {
     }
 
     private void createMap(){
+       
+        
         for (int i = 0; i < mapSize; i++) {
             for (int j = 0; j < mapSize; j++) {
                 Vector2Int vector = new Vector2Int(i, j);
                 this.grid.Add(vector, "_");
             }
         }
-
+        
         setFinishTarget();
         setPlayer();
+        for (int i = 0; i < this.deathTiles; i++) {
+            setDanger();
+        }
     }
     private void setFinishTarget()
     {
-        int x = Random.Range(0, mapSize);
-        int y = Random.Range(0, mapSize);
+        int x = UnityEngine.Random.Range(0, mapSize);
+        int y = UnityEngine.Random.Range(0, mapSize);
 
-        this.grid[new Vector2Int(x, y)] = "T";
+        Vector2Int position = new Vector2Int(x, y);
+
+        this.grid[position] = "T";
+        this.treasurePosition = position;
 
         Debug.Log($"treasure is in {x},{y}: {this.getTileInfo(x,y)}");
 
@@ -64,16 +82,50 @@ public class Map : MonoBehaviour {
 
     private void setPlayer()
     {
-        int x = Random.Range(0, mapSize);
-        int y = Random.Range(0, mapSize);
+        int tries = 0;
+        Vector2Int playerPosition;
+        do {
+            int x = UnityEngine.Random.Range(0, mapSize);
+            int y = UnityEngine.Random.Range(0, mapSize);
+            playerPosition = new Vector2Int(x, y);
+            tries++;
 
-        Vector2Int playerPosition = new Vector2Int(x, y);
+            if(tries > MAX_TRIES_FOR_MAP_GENERATION){
+                throw new System.Exception("MAX NUMBER OF MAP GENERATION TRIES REACHED");
+            }
+
+        } while(!this.isPlayerTheoricalSpawnPositionCorrect(playerPosition));
 
         this.grid[playerPosition] = "P";
 
         this.playerPosition = playerPosition;
 
-        Debug.Log($"Player is in {x},{y}: {this.getTileInfo(x, y)}");
+        Debug.Log($"Player is in {playerPosition}: {this.getTileInfo(playerPosition)}");
+
+    }    
+    
+    private void setDanger()
+    {
+        int tries = 0;
+        Vector2Int dangerPosition;
+        this.dangerPositions = new List<Vector2Int>();
+        do {
+            int x = UnityEngine.Random.Range(0, mapSize);
+            int y = UnityEngine.Random.Range(0, mapSize);
+            dangerPosition = new Vector2Int(x, y);
+            tries++;
+
+            if(tries > MAX_TRIES_FOR_MAP_GENERATION ){
+                throw new System.Exception("MAX NUMBER OF MAP GENERATION TRIES REACHED");
+            }
+
+        } while(!this.isDangerTheoricalSpawnPositionCorrect(dangerPosition));
+
+        this.grid[dangerPosition] = "D";
+
+        this.dangerPositions.Add(playerPosition);
+
+        Debug.Log($"Dangers are in {this.dangerPositions}");
 
     }
 
@@ -115,8 +167,47 @@ public class Map : MonoBehaviour {
         this.mapSize = mapSize;
     }
 
+    public void revealAll(){
+        for (int i = 0; i < mapSize; i++) {
+            for (int j = 0; j < mapSize; j++) {
+                Vector2Int vector = new Vector2Int(i,j);
+                tileMapController.DrawTile(vector, grid[vector]);
+            }
+        }
+    }
 
+    private bool isPlayerTheoricalSpawnPositionCorrect(Vector2Int possiblePlayerPosition){
 
+        Debug.Log($"Treasure is im {treasurePosition}");
+        Debug.Log($"Is Player able to spawn in {possiblePlayerPosition}");
+        float diff = Mathf.Abs(treasurePosition.sqrMagnitude - possiblePlayerPosition.sqrMagnitude);
+        Debug.Log($"SQR MAGNITUDE DIFF IS {diff.ToString()}"); 
+        if (diff < 16) {
+            Debug.Log($"PLAYER IS TOO CLOSE TO TREASURE {diff.ToString()}");
+            return false;
+        }
+        return true;
+
+    }
+    private bool isDangerTheoricalSpawnPositionCorrect(Vector2Int possibleDangerPosition){
+
+        float diff = 99;
+        foreach (Vector2Int dangerPosition in this.dangerPositions) {
+            diff = Mathf.Abs(dangerPosition.sqrMagnitude - possibleDangerPosition.sqrMagnitude);
+            if (diff < 5) {
+                Debug.Log($"DANGER IS TOO CLOSE TO OTHER DANGER {diff.ToString()}");    
+                return false;
+            }
+        }
+        Debug.Log($"Player is in {this.playerPosition}");
+        diff = Mathf.Abs(this.playerPosition.sqrMagnitude - possibleDangerPosition.sqrMagnitude);
+        Debug.Log($"SQR MAGNITUDE DIFF IS {diff.ToString()}"); 
+        if (diff < 16) {
+            Debug.Log($"DANGER IS TOO CLOSE TO PLAYER {diff.ToString()}");
+            return false;
+        }
+        return true;
+    }
 
 
 
