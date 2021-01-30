f1f408e798f50a212068c491fd8cbe2d2eac18f8
Emiliano Cenizo <emicenizo@gmail.com>
2021-01-30 23:33:29 +0100

    UI items place

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -264,7 +264,7 @@ public class Map : MonoBehaviour {
     public Item openChest(Vector2Int position){
         this.grid[position] = TileType.TileTypes.OPEN_CHEST;
         tileMapController.DrawTile(position, TileType.TileTypes.OPEN_CHEST);
-        return Item.createItem(Item.ItemKind.FLYING_PIDGEON);
+        return Item.createItem(Item.ItemKind.HOMING_PIDGEON);
     }
 
 
